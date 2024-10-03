e208507397781604cca384360c44cc00e23c8a44
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2024-10-03 14:46:41 +0100

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from "react";
+import { Box } from "@radix-ui/themes";
+import Layout from "./components/Layout";
+import CreateProposalModal from "./components/CreateProposalModal";
+import {
+    useAppKitAccount,
+    useAppKitProvider,
+    useWalletInfo,
+} from "@reown/appkit/react";
+import Proposals from "./components/Proposals";
+import useContract from "./hooks/useContract";
+import useFetchProposals from "./hooks/useFetchProposals";
+
+
+
+function App() {
+    const { walletProvider } = useAppKitProvider("eip155");
+    const { walletInfo } = useWalletInfo();
+    const { address, status, isConnected } = useAppKitAccount();
+    console.log("walletInfo: ", walletInfo);
+    console.log("walletInfo: ", address, status, isConnected);
+
+    console.log("walletProvider: ", walletProvider, useAppKitProvider);
+
+    const readOnlyProposalContract = useContract();
+    
+
+    const { proposals } = useFetchProposals();
+
+   
+
+    return (
+        <Layout>
+            <Box className="flex justify-end p-4">
+                <CreateProposalModal />
+            </Box>
+            <Box className="">
+                <Proposals proposals={proposals} />
+            </Box>
+        </Layout>
+    );
+}
+
+export default App;
